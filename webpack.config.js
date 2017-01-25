721346af5a11f65015d58f87e1fc6657a581ba81
Vinicius Tavares <vinicius.rafael19@hotmail.com>
2017-01-25 18:25:43 -0200

    removing bundle files from repository

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify('production')
     }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
